60e8a72ad9b7a8c35b095db8b0314522fda7f173
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-10-23 00:02:48 +0300

    Disable hash generation

--- config-overrides.js
+++ config-overrides.js
@@ -5,6 +5,14 @@ module.exports = (config, env) => {
       default: false,
     },
   };
+  /**
+   * Disable hash generation in production build for JS
+   */
+  config.output.filename = "static/js/[name].bundle.js";
+  /**
+   * Disable hash generation in production build for CSS
+   */
+  config.plugins[5].options.filename = "static/css/[name].bundle.css";
   config.externals = {
     react: 'React',
     'react-dom': 'ReactDOM'
